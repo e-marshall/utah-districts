8e2f00ab873cf23d29fcd23c250696ac6f090598
Emma Marshall <emarshall@rhg.com>
2025-08-28 11:31:37 -0600

    old version back on main

--- tests/test_organize_maps.py
+++ tests/test_organize_maps.py
@@ -1,18 +1,17 @@
 import pytest
 from utah_districts.organize_maps import create_map_unit, MapUnit
 
-def test_create_map():
 
-    for mtype in ['enacted','proposed']:
-        obj = create_map_unit(state='Utah',
-                              mtype=mtype)
+def test_create_map():
+    for mtype in ["enacted", "proposed"]:
+        obj = create_map_unit(state="Utah", mtype=mtype)
         assert obj.map_type == mtype
         assert obj.map_name == mtype
 
-def test_maps():
 
+def test_maps():
     map_unit = MapUnit(
         state="Utah",
         mtype="enacted",
-        map_name = "enacted",
+        map_name="enacted",
     )
